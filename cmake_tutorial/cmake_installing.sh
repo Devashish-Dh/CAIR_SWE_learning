3bdd84e57786d819fd5997da06761b2401608247
Devashish-Dh <devashish.dhaulakhandi@gmail.com>
2024-07-31 22:19:45 +0530

    learning how to use cmake to build c++ projects, this was a basic hello_world project/program

---
+++ cmake_tutorial/cmake_installing.sh
@@ -0,0 +1,10 @@
+#C/C++ does not have a package manager like pip in python, but we still have a way to setup and "make" the libraries/code written by others work together with our own project using this.. maybe
+
+# it also allows our project to become portable... how ? will see 
+
+# on linux : sudo apt-cache policy cmake
+# this checks what ver our ubuntu package manager has of cmake
+
+# install :sudo apt install cmake
+
+
---
+++ cmake_tutorial/simple_cmake_example/hi.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+	
+	std::cout<<"hello! this is the main_file of my hello world \n";
+
+return 0;
+}
