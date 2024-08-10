d564ed1e2187bb6c15cdd11603acb8fed0ac6d60
Devashish-Dh <devashish.dhaulakhandi@gmail.com>
2024-08-10 14:00:50 +0530

    restructured and renamed to track learned what when

---
+++ Code_Tech_and_Tutorials_CMake/3_somelibdemo_Static_lib_and_GIT_submodule_to_use_a_repo_lib/somelibdemo_as_static_lib/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "../libs/somelibdemo/somelibdemo.h" //this is to be done by CMake but we are doing it manually the first time, so we need to give the header files of the library used to the main.cpp so that it knows where to look for the declarations and link the files correctly
+
+int main(){
+
+std::cout<<devmath::add(123,2);
+
+return 0;
+}
